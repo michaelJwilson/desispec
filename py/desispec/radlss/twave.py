a2fb28c98f527bb6435219a5cadf9b890ad3c86f
michaelJwilson <mjwilson@lbl.gov>
2021-01-21 14:32:17 -0800

    Remove git copies.

--- py/desispec/radlss/twave.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def twave(tmin, tmax, dtwave=0.4, shift=5.):
-    return  np.arange(-shift + tmin, shift + dtwave + tmax, dtwave)
-
-if __name__ == '__main__':
-    twave = twave(5., 15., dtwave=0.4, shift=5.)
-
-    print(twave)
